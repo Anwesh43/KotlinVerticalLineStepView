4777a8f2cd011548d43d138f375ed27485e2a058
Anwesh43 <anweshthecool0@gmail.com>
2018-04-05 01:12:15 +0530

    adding Animator class

--- verticallineview/src/main/java/ui/anwesome/com/verticallineview/VerticalLineStepView.kt
+++ verticallineview/src/main/java/ui/anwesome/com/verticallineview/VerticalLineStepView.kt
@@ -42,4 +42,32 @@ class VerticalLineStepView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator (var view : View, var animated : Boolean = false) {
+        fun animate (updatecb : () -> Unit) {
+            if (animated) {
+                try {
+                    updatecb()
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start () {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop () {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
