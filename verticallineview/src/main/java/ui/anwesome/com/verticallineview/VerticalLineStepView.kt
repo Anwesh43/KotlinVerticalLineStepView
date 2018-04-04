3ebc6c751d0fdf89d8c859b8271e0129be86c8a9
Anwesh43 <anweshthecool0@gmail.com>
2018-04-05 01:06:25 +0530

    adding VerticalLineStepView class

--- verticallineview/src/main/java/ui/anwesome/com/verticallineview/VerticalLineStepView.kt
+++ verticallineview/src/main/java/ui/anwesome/com/verticallineview/VerticalLineStepView.kt
@@ -3,3 +3,24 @@ package ui.anwesome.com.verticallineview
 /**
  * Created by anweshmishra on 05/04/18.
  */
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
+class VerticalLineStepView (ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
