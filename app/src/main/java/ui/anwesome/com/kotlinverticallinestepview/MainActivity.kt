da425cc3f00e9331d188c338ed93ea338cb2d9ad
Anwesh43 <anweshthecool0@gmail.com>
2018-04-05 02:32:44 +0530

    adding VerticalLiNEsTEPvIEW IN aCTIVITY

--- app/src/main/java/ui/anwesome/com/kotlinverticallinestepview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinverticallinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinverticallinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.verticallineview.VerticalLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        VerticalLineStepView.create(this)
     }
 }
